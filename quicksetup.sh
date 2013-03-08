b7a70ce55be01ae50e642b2b5098f5c3d232432e
Royans Tharakan <royans@google.com>
2013-03-08 10:38:49 -0800

    make it easier to launch from shell.

---
+++ quicksetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+  echo "Usage: ./quick.sh <email@address>"
+  echo "       - The email address should belong to the user who needs to get the onc certificate."
+  echo "       - For example: ./quick.sh test@blogofy.com"
+  echo ""
+  exit 1
+fi
+
+echo "Downloading..."
+curl https://nodeload.github.com/royans/ec2_chromeos_openvpn/zip/master > m.zip 2> /dev/null
+rm -rf ec2_chromeos_openvpn-master 2> /dev/null
+unzip m.zip
+rm m.zip
+cd ec2_chromeos_openvpn-master/
+./setup.sh $1
+cd ..
+rm -rf ec2_chromeos_openvpn-master
